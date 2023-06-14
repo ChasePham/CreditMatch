2ba031769af65be8d367cb5caf89b381437ee020
ChasePham <chase9659@gmail.com>
2023-06-14 14:09:51 -0500

    Chase variables

--- chase.go
+++ chase.go
@@ -2,10 +2,19 @@ package chase
 
 
 type Chase struct {
+	flex_cat float64 
+	travel_portal_purchases float64
+	other_travel_purchases float64
+	drug_stores float64
+	restaurants float64
+	online_groceries float64
+	streaming_services float64
+	other_purchases float64
 
 }
 
 func (Chase) freedomflex() float64 { 
+	
 }
 
 
@@ -18,5 +27,5 @@ func (Chase) sapphirePreferred() float64 {
 }
 
 func (Chase) sapphireReserved() float64 {
-	
+
 }
