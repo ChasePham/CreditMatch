71f87cea681cd50f2738da6763fcfd764a1eaf2c
ChasePham <chase9659@gmail.com>
2023-06-14 18:36:59 -0500

    capital_one

--- capitalone.go
+++ capitalone.go
@@ -17,5 +17,9 @@ func (c capital) savor() float64 {
 }
 
 func (c capital) venturex() float64 {
-	
+	return c.hotels_and_car_rentals * 10 * .01 + c.flights * 5 * .01 + other_purchases * 2 * .01 + (300 + 100 - 395)
+}
+
+func (c capital) total() float64 {
+	return savor() + venturex()
 }
