7ef1e76ab846247e8ab73f2ebe1dc13bb9201dd3
ChasePham <chase9659@gmail.com>
2023-06-14 13:24:26 -0500

    Chase

--- chase.go
+++ chase.go
@@ -1,1 +1,22 @@
 package chase
+
+
+type Chase struct {
+
+}
+
+func (Chase) freedomflex() float64 { 
+}
+
+
+func (Chase) freedomUnlimited() float64 {
+
+}
+
+func (Chase) sapphirePreferred() float64 {
+
+}
+
+func (Chase) sapphireReserved() float64 {
+	
+}
