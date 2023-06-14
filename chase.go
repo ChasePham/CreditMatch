28ca2eefefb864b2a045f3b7dfe281855c078909
ChasePham <chase9659@gmail.com>
2023-06-14 16:38:43 -0500

    sapphireReserved

--- chase.go
+++ chase.go
@@ -1,5 +1,6 @@
 package chase
 
+import "math"
 
 type Chase struct {
 	flex_cat float64 
@@ -29,7 +30,7 @@ func (c Chase) sapphirePreferred() float64 {
 	var temp_travel float64 = c.travel_portal_purchases
 	temp_travel -= 50
 
-	var total_points float64 = temp_travel * .5
+	var total_points float64 = math.Max(0,temp_travel * 5)
 	total_points += c.other_travel_purchases * 2
 	total_points += c.restaurants * 3
 	total_points += c.streaming_services * 3
@@ -51,9 +52,35 @@ func (c Chase) sapphirePreferred() float64 {
 	}
 
 	// Travel portal is already discounted within this method
-	return total_points * .01 - temp_travel
+	return total_points * .01 - math.Max(0,temp_travel)
 }
 
 func (c Chase) sapphireReserved() float64 {
+	var total float64 = -550
+	var temp_travel float64 = c.travel_portal_purchases
+	temp_travel -= 300
 
+	var total_points float64 = math.Max(0,temp_travel * 5)
+	total_points += c.other_travel_purchases * 2
+	total_points += c.restaurants * 3
+	total_points += c.streaming_services * 3
+	total_points += c.online_groceries * 3
+	var total_spent float64 = temp_travel + c.other_travel_purchases + c.restaurants + c.streaming_services + c.online_groceries
+	total_points += total_spent * .1
+
+
+
+	var additional_value float64 = temp_travel / .0125
+
+	if additional_value > total_points {
+		additional_value -= total_points
+		total_points = 0
+		temp_travel -= additional_value * .0125
+	} else {
+		total_points -= additional_value
+		temp_travel = 0
+	}
+
+	// Travel portal is already discounted within this method
+	return total_points * .01 - math.Max(0,temp_travel)
 }
