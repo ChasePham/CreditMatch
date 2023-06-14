57beb8937e1128f4713a6704f2c3b6245da138a5
ChasePham <chase9659@gmail.com>
2023-06-14 16:01:08 -0500

    sapphirePref

--- chase.go
+++ chase.go
@@ -19,12 +19,26 @@ func (c Chase) freedomflex() float64 {
 
 
 func (c Chase) freedomUnlimited() float64 {
-	return other_travel_purchases * .05 + drug_stores * .03
+	return c.other_purchases * .05 + c.drug_stores * .03
 }
 
 func (c Chase) sapphirePreferred() float64 {
 	var total float64 = -95.0
-	total += 50
+	var temp_travel float64 = c.travel_portal_purchases
+	temp_travel -= 50
+
+	var total_points float64 = temp_travel * .5
+	total_points += c.other_travel_purchases * 2
+	total_points += c.restaurants * 3
+	total_points += c.streaming_services * 3
+	total_points += c.online_groceries * 3
+	var total_spent float64 = temp_travel + c.other_travel_purchases + c.restaurants + c.streaming_services + c.online_groceries
+	total_points += total_spent * 10
+
+	
+
+
+
 	
 }
 
