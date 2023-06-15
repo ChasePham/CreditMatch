007acdbbf4ab0f235ecfa746cd4fd10f65d4e37c
ChasePham <chase9659@gmail.com>
2023-06-15 12:27:45 -0500

    amex variables

--- amex.go
+++ amex.go
@@ -1,5 +1,13 @@
 package amex
 
 type amex struct {
-	
+	supermarkets float64
+	online_retail float64
+	gas_stations float64
+	restaurants float64
+	flights_portal float64
+	hotels float64
+	ubers float64
+	digital_entertainment float64
 }
+
