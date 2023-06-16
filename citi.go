d854da50a59fbe628ac94e6d50a503a0bba0e989
ChasePham <chase9659@gmail.com>
2023-06-16 16:37:56 -0500

    double cash and customnCash

--- citi.go
+++ citi.go
@@ -1,1 +1,24 @@
 package citi
+
+
+
+
+type citi struct {
+	other_purchases float64
+	custom_categories float64
+
+}
+
+func (c citi) doubleCash() float64 {
+	return c.other_purcahses * .02
+}
+
+func (c citi) customCash() float64 {
+	var result float64 = 0
+	result += 6000 * 5 * .01 + (c.custom_categories - 6000) * .01
+	return result
+}
+
+func (c citi) premier() float64 {
+
+}
