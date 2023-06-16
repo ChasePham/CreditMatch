73faf19978b6125b44a828e317613d6e756eec2e
ChasePham <chase9659@gmail.com>
2023-06-16 16:44:37 -0500

    premier

--- citi.go
+++ citi.go
@@ -1,11 +1,14 @@
 package citi
 
+import "math"
 
 
 
 type citi struct {
 	other_purchases float64
 	custom_categories float64
+	travel_portal_purchases float64 // Excludes air travel ONLY
+	other_traveL_purchases float64 // Includes air travel with everything else from portal purchases
 
 }
 
@@ -20,5 +23,7 @@ func (c citi) customCash() float64 {
 }
 
 func (c citi) premier() float64 {
-
+	var result float64 = 0
+	var remaining_portal_purchases float64 = math.Max(0,c.travel_portal_purchases - 100)
+	return remaining_portal_purchases * 10 * .01 + c.other_travel_purchases * 3 * .01
 }
